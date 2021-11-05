0ca73d2211e33e5b5820a61502f68b91c619ae2d
Francisca Osores <fosores@linets.cl>
2021-11-04 21:10:26 -0300

    wip

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Form = () => {
+  const [appointment, updateAppointment] = useState({
+    pet: "",
+    owner: "",
+    discharge_date: "",
+    discharge_time: "",
+    symptoms: "",
+  });
+
+  const handleChange = e => {
+    updateAppointment({
+        ...appointment,
+        [e.target.name]: e.target.value
+    })
+  };
+
+  const {pet, owner, discharge_date, discharge_time, symptoms} = appointment
+
   return (
     <Fragment>
       <h2>Crear cita</h2>
@@ -8,38 +25,51 @@ const Form = () => {
         <label>Nombre Mascota</label>
         <input
           type="text"
-          name="mascota"
+          name="pet"
           className="u-full-width"
           placeholder="Nombre Mascota"
+          onChange={handleChange}
+          value={pet}
         />
 
         <label>Nombre dueño</label>
         <input
           type="text"
-          name="dueño"
+          name="owner"
           className="u-full-width"
           placeholder="Nombre Dueño"
+          onChange={handleChange}
+          value={owner}
         />
 
         <label>Fecha de alta</label>
-        <input type="date" name="fecha de alta" className="u-full-width" />
+        <input
+          type="date"
+          name="discharge_date"
+          className="u-full-width"
+          onChange={handleChange}
+          value={discharge_date}
+        />
 
         <label>Hora de alta</label>
         <input
-            type="time"
-            name="hora de alta"
-            className="u-full-width" />
+          type="time"
+          name="discharge_time"
+          className="u-full-width"
+          onChange={handleChange}
+          value={discharge_time}
+        />
 
-        <label>Fecha de alta</label>
-        <textarea
+        <label>Sintomas</label>
+        <textarea 
             className="u-full-width"
+            name="symptoms"
+            onChange={handleChange}
+            value={symptoms}
         ></textarea>
 
-        <button
-            type="submit"
-            className="u-full-width button-primary"
-        >
-            Agregar cita
+        <button type="submit" className="u-full-width button-primary">
+          Agregar cita
         </button>
       </form>
     </Fragment>
